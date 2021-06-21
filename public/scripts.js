9b5b11b64e5d5378b78f906cf678f2adf3f1bbff
thiennd <thien.nd3589@gmail.com>
2021-06-21 14:50:30 +0700

    init

---
+++ public/scripts.js
@@ -0,0 +1,23 @@
+const sendButton = document.getElementById("send");
+
+sendButton.addEventListener("click", () => {
+    console.log("click");
+    const acc = document.getElementById("stk").value;
+    const bank = document.getElementById("bank").value;
+    const body = JSON.stringify({ acc, bank });
+    fetch("http://localhost:8080/send", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: body,
+    }).then(() => {
+        const hidable = document.getElementsByClassName("hidable");
+        for (let i = 0; i < hidable.length; i++) {
+            hidable.item(i).classList.toggle("hide");
+        }
+
+        const phead =  document.getElementById('head');
+        phead.textContent = 'Check tài khoản nha!'
+    });
+});
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.post("/send", (req, res) => {
+    const info = req.body;
+    console.log(info)
+    res.send();
+});
+
+app.listen(8080, () => {
+    console.log("Server is listening on port 8080");
+});
